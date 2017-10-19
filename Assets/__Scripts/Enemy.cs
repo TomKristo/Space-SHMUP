9b135847e1ea4347ac60f8ab64a04d423c66ae81
Tomislav Kristo <Tomkristo@gmail.com>
2017-10-19 18:03:33 -0400

    Added enemy script

---
+++ Assets/__Scripts/Enemy.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public float speed = 10f;
+    public float fireRate = 0.3f;
+    public float health = 10;
+    public int score = 100;
+
+    public bool ____________________;
+
+    public Bounds bounds;
+    public Vector3 boundsCenterOffset;
+
+    private void Awake()
+    {
+        InvokeRepeating("CheckOffscreen", 0f, 2f);
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+
+    public Vector3 pos
+    {
+        get
+        {
+            return (this.transform.position);
+        }
+
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+
+    void CheckOffscreen()
+    {
+        if(bounds.size == Vector3.zero)
+        {
+            bounds = Utils.CombineBoundsOfChildren(this.gameObject);
+            boundsCenterOffset = bounds.center - transform.position;
+        }
+
+        bounds.center = transform.position + boundsCenterOffset;
+        Vector3 off = Utils.ScreenBoundsCheck(bounds, BoundsTest.offScreen);
+
+        if(off.y < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
