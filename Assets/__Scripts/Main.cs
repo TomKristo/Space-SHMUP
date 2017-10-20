a1dae74ecc64ecb885a67ee647e8893d16611bfb
Tomislav Kristo <Tomkristo@gmail.com>
2017-10-19 22:47:02 -0400

    Added Projectile and scripts

--- Assets/__Scripts/Main.cs
+++ Assets/__Scripts/Main.cs
@@ -6,20 +6,47 @@ public class Main : MonoBehaviour {
 
     static public Main S;
 
+    static public Dictionary<WeaponType, WeaponDefinition> W_DEFS;
     public GameObject[] prefabEnemies;
     public float enemySpawnPerSecond = 0.5f;
     public float enemySpawnPadding = 1.5f;
+    public WeaponDefinition[] weaponDefinitions;
 
     public bool _________________;
 
+    public WeaponType[] activeWeaponsTypes;
     public float enemySpawnRate;
 
-    private void Awake()
+    void Awake()
     {
         S = this;
         Utils.SetCameraBounds(this.GetComponent<Camera>());
         enemySpawnRate = 1f / enemySpawnPerSecond;
         Invoke("SpawnEnemy", enemySpawnRate);
+
+        W_DEFS = new Dictionary<WeaponType, WeaponDefinition>();
+        foreach(WeaponDefinition def in weaponDefinitions)
+        {
+            W_DEFS[def.type] = def;
+        }
+    }
+
+    static public WeaponDefinition GetWeaponDefinition(WeaponType wt)
+    {
+        if (W_DEFS.ContainsKey(wt))
+        {
+            return (W_DEFS[wt]);
+        }
+        return (new WeaponDefinition());
+    }
+
+    void Start()
+    {
+        activeWeaponsTypes = new WeaponType[weaponDefinitions.Length];
+        for (int i = 0; i < weaponDefinitions.Length; i++)
+        {
+            activeWeaponsTypes[i] = weaponDefinitions[i].type;
+        }
     }
 
     public void SpawnEnemy()
